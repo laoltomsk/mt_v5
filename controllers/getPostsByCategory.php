158c57262a010b14f73aeea2cc7a6a15895d1e4e
DNS <DNS@DESKTOP-V3EES5J>
2019-05-01 14:14:47 +0700

    Реализован вывод новостей по категориям с сохранением нышених (довольно странных) URL.

---
+++ controllers/getPostsByCategory.php
@@ -0,0 +1,23 @@
+<?php
+require_once("../settings.php");
+require_once("../models/newsmodel.php");
+require_once("../views/category_view.php");
+
+$model = new NewsModel();
+$view = new CategoryView();
+
+$data = array();
+
+if ($_GET['category'] === "main") {
+    $_GET['category'] = "news";
+}
+
+if ($_GET['category'] === "obzor") {
+    $_GET['category'] = "reviews";
+}
+
+$data['news'] = $model->getData($db, $_GET['category'], '', infinity, $_GET['page'] * newsOnPage);
+$data['category'] = $_GET['category'];
+
+$view->show($data);
+?>
--- models/newsmodel.php
+++ models/newsmodel.php
@@ -2,27 +2,26 @@
 require_once("model.php");
 
 class NewsModel extends Model {
-    public function getData($db, $category = '', $tag = '', $from = 999999999999, $page = 0) {
+    public function getData($db, $category = '', $tag = '', $from = infinity, $offset = 0, $onpage = newsOnPage) {
         $news = array();
 
-        $offset = $page * 20;
         $categoryFilter = '';
         if ($category !== '') {
-            $categoryFilter = "AND `category` = `$category`";
+            $categoryFilter = "AND `tb_uri`.`category` = '$category'";
         }
         if ($tag === '') {
             $queryResults = $db->query("SELECT * FROM `tb_uri`
-              LEFT JOIN `tb_pages_viewed` ON `tb_uri`.`uri` = `tb_pages_viewed`.`uri` ".$categoryFilter."
-              WHERE `tb_uri`.`uri` < {$from}
-              ORDER BY `tb_uri`.`uri` DESC LIMIT {$offset}, 20");
+              LEFT JOIN `tb_pages_viewed` ON `tb_uri`.`uri` = `tb_pages_viewed`.`uri`
+              WHERE `tb_uri`.`uri` < {$from} ".$categoryFilter."
+              ORDER BY `tb_uri`.`uri` DESC LIMIT {$offset}, {$onpage}");
         }
         else {
             $queryResults = $db->query("SELECT * FROM `tb_uri`
-              LEFT JOIN `tb_pages_viewed` ON `tb_uri`.`uri` = `tb_pages_viewed`.`uri` ".$categoryFilter."
+              LEFT JOIN `tb_pages_viewed` ON `tb_uri`.`uri` = `tb_pages_viewed`.`uri`
               INNER JOIN `tb_pages` ON `tb_uri`.`id` = `tb_pages`.`uri_id`
               INNER JOIN `tb_tag` ON `tb_pages`.`tag_id` = `tb_tag`.`id` AND `tb_tag`.`name` = '{$tag}'
-              WHERE `tb_uri`.`uri` < {$from}
-              ORDER BY `tb_uri`.`uri` DESC LIMIT {$offset}, 20");
+              WHERE `tb_uri`.`uri` < {$from} ".$categoryFilter."
+              ORDER BY `tb_uri`.`uri` DESC LIMIT {$offset}, {$onpage}");
         }
 
         while ($queryResult = $queryResults->fetch_array()) {
