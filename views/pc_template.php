58ef50b6844552f4c25bf5e2b91eac1cf82c8d15
DNS <DNS@DESKTOP-V3EES5J>
2019-05-09 20:59:13 +0700

    Начал верстать главную страницу. Почему-то идёт очень медленно...

--- views/pc_template.php
+++ views/pc_template.php
@@ -30,57 +30,50 @@ class PCTemplate extends Template {
             <meta name="keywords" content="<?php echo siteKeyWords ?>">
             <meta name="robots" content="index,follow">
             <meta name="google-site-verification" content="UDX_nuGeNzWBLXRAwO_a5dCVRSMTXIYW1E_BelG9a24" />
-            <!--<meta name="theme-color" content="#1180aa">
+            <meta name="theme-color" content="#1180aa">
             <meta name="msapplication-navbutton-color" content="#1180aa">
             <meta name="apple-mobile-web-app-capable" content="yes">
-            <meta name="apple-mobile-web-app-status-bar-style" content="#1180aa">-->
+            <meta name="apple-mobile-web-app-status-bar-style" content="#1180aa">
+            <link rel="icon" type="image/x-icon" href="/assets/pc/favicon.ico">
+            <link rel="shortcut icon" type="image/x-icon" href="/assets/pc/favicon.ico" />
+            <link rel="icon" sizes="192x192" href="/assets/pc/touch-icon-192x192.png">
+            <link rel="apple-touch-icon-precomposed" sizes="180x180" href="/assets/pc/apple-touch-icon-180x180-precomposed.png">
+            <link rel="apple-touch-icon-precomposed" sizes="152x152" href="/assets/pc/apple-touch-icon-152x152-precomposed.png">
+            <link rel="apple-touch-icon-precomposed" sizes="144x144" href="/assets/pc/apple-touch-icon-144x144-precomposed.png">
+            <link rel="apple-touch-icon-precomposed" sizes="120x120" href="/assets/pc/apple-touch-icon-120x120-precomposed.png">
+            <link rel="apple-touch-icon-precomposed" sizes="114x114" href="/assets/pc/apple-touch-icon-114x114-precomposed.png">
+            <link rel="apple-touch-icon-precomposed" sizes="76x76" href="/assets/pc/apple-touch-icon-76x76-precomposed.png">
+            <link rel="apple-touch-icon-precomposed" sizes="72x72" href="/assets/pc/apple-touch-icon-72x72-precomposed.png">
+            <link rel="apple-touch-icon-precomposed" href="/assets/pc/apple-touch-icon-precomposed.png">
+            <link href="https://fonts.googleapis.com/css?family=Noto+Sans:400,400i,700,700i&amp;subset=cyrillic,cyrillic-ext,greek,greek-ext,latin-ext" rel="stylesheet">
+            <link rel="stylesheet" href="/styles/style.css">
 
-            <script> //находится здесь временно, до этапа вёрстки
-                function LoadMoreFromIndexPage(from) {
-                    let xhr = new XMLHttpRequest();
-                    xhr.open('get', '/controllers/getMorePosts.php?from='+from)
-                    xhr.send();
-                    xhr.onload = function() {
-                        document.getElementById("news").innerHTML += xhr.responseText;
-                        let newsCount = document.getElementsByTagName("h3").length;
-                        let id = document.getElementsByTagName("h3")[newsCount-1].getAttribute("data-id");
-                        document.getElementsByTagName("button")[0].setAttribute("onclick", "LoadMoreFromIndexPage("+id+")");
-                    }
-                }
-
-                function LoadMoreFromCategory(from, category) {
-                    let xhr = new XMLHttpRequest();
-                    xhr.open('get', '/controllers/getMorePostsByCategory.php?from='+from+'&category='+category);
-                    xhr.send();
-                    xhr.onload = function() {
-                        document.getElementById("news").innerHTML += xhr.responseText;
-                        let newsCount = document.getElementsByTagName("h3").length;
-                        let id = document.getElementsByTagName("h3")[newsCount-1].getAttribute("data-id");
-                        document.getElementsByTagName("button")[0].setAttribute("onclick", "LoadMoreFromCategory("+id+",'"+category+"')");
-                    }
-                }
-
-                function LoadMoreByTag(from, category, tag) {
-                    let xhr = new XMLHttpRequest();
-                    xhr.open('get', '/controllers/getMorePostsByTag.php?from='+from+'&category='+category+'&tag='+tag);
-                    xhr.send();
-                    xhr.onload = function() {
-                        document.getElementById("news").innerHTML += xhr.responseText;
-                        let newsCount = document.getElementsByTagName("h3").length;
-                        let id = document.getElementsByTagName("h3")[newsCount-1].getAttribute("data-id");
-                        document.getElementsByTagName("button")[0].setAttribute("onclick", "LoadMoreFromCategory("+id+",'"+category+"'&tag='"+tag+"')");
-                    }
-                }
-
-                function AddView(id) {
-                    let xhr = new XMLHttpRequest();
-                    xhr.open('get', '/controllers/addViewToPost.php?id='+id);
-                    xhr.send();
-                }
-            </script>
+            <script src="/scripts/loadMoreScripts.js"></script>
         </head>
         <body>
-        <h1>(десктопная версия)</h1>
+        <div id="main-wrapper">
+            <header>
+                <div class="social_btns">
+                    <span class="social_btn" id="youtube_btn">YT</span>
+                    <span class="social_btn" id="telegram_btn">TG</span>
+                    <span class="social_btn" id="vk_btn">VK</span>
+                    <span class="social_btn" id="instagram_btn">IG</span>
+                    <span class="social_btn" id="facebook_btn">FB</span>
+                    <span class="social_btn" id="twitter_btn">TW</span>
+                </div>
+                <div class="logo">
+                    <a href="/"><img src="/assets/pc/logo.png" alt="mobiltelefon.ru logo"></a>
+                </div>
+                <div class="categories">
+                    <a href="/contents_main_0.html" class="category">Новости</a>
+                    <a href="/contents_obzor_0.html" class="category">Обзоры</a>
+                    <a href="/contents_blog_0.html" class="category">Статьи</a>
+                    <a href="#" class="category inactive">Каталог</a>
+                    <a href="#" class="category inactive">Видео</a>
+                    <a href="#" class="category tag">#mwc2019</a>
+                    <input>
+                </div>
+            </header>
         <?php switch ($category) {
             case "news":
                 ?> <b>Новости</b> --- <a href="/contents_obzor_0.html">Обзоры</a> --- <a href="/contents_blog_0.html">Статьи</a>
@@ -102,6 +95,7 @@ class PCTemplate extends Template {
         ?>
         <p><a href="/sendtip.html">Прислать новость</a></p>
         <p><b>Mobiltelefon.ru, 2019</b></p>
+        </div>
         </body>
         </html>
         <?php
